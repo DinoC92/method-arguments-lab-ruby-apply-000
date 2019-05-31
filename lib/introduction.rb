d62788c2baa5805c1e3152e0290820a2d023a56e
Dean Burgess <dinocastiglione@ymail.com>
2019-05-31 16:42:28 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+# lib/introduction.rb 
+
+def introduction(name)
+  puts "Hi, my name is Dino
+  {name}."
+end
+#introduction_with_language
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{Dino} and I am learning to program in #{ruby}"
+  {name}
+  {language}
+end 
